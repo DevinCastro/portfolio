4f6b8d9d93d0598cbc7153445e51f2fa7ca20789
Devin Castro <dcrocker21@gmail.com>
2020-10-08 00:50:04 -0700

    added tech skills

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,19 +18,19 @@ const Home = () => {
               <p>And I play guitar.</p>
             </div>
 
-      
-          <div className="text-center">
-            <img className="guitarPhoto"src={me} alt="me"/>
-          </div>
 
-          <br/><br/>
+            <div className="text-center">
+              <img className="guitarPhoto" src={me} alt="me" />
+            </div>
+
+            <br /><br />
 
 
           </Container>
 
           <div className="blue text-center">
             <div className="bio">
-            <h3>Thanks for stopping by.</h3>
+              <h3>Thanks for stopping by.</h3>
               <p className="aboutMe">Full Stack Web Developer with a Bachelor's degree in Mathematics having an immense passion for working collaboratively to solve problems. Recently obtained a certificate in full stack web development at the UCI Coding Bootcamp. Ambitious and ready to handle accelerated learning, fast paced work flow, and always meet deadlines with methodical organization and planning. Goal-oriented team player that thrives on clear communication.</p>
             </div>
           </div>
@@ -39,25 +39,58 @@ const Home = () => {
 
             <Jumbotron className="jumbotron">
               <Row>
-                <h1 ></h1>
+                <h1></h1>
                 <Col md="4 text-center">
-                  <h1>one</h1>
+                  <h1>Front End</h1>
+                  <p>
+                    Web Frameworks:
+                    <br/>
+                    HTML5, CSS3
+                  <br />
+                  <br />
+                  Scripts (client-side):
+                  <br/>
+                  JavaScript, jQuery, React.js
+                  <br/>
+                  AJAX, RESTful API’s
+                  <br/>
+                  <br/>
+                  UI frameworks:
+                  <br/>
+Bootstrap, React UI Components
+                  </p>
                 </Col>
 
                 <Col md="4 text-center">
-                  <h1>one</h1>
+                  <h1>Back End</h1>
+                  <p>
+                    Scripts (server-side):
+                    <br/>
+                    Node.js, Express.js
+                    <br/>
+                    <br/>
+                    Node Package Managers:
+                    <br/>
+
+                    Express, Sequelize, Express-handlebars, 
+                    <br/>
+                    Mongoose, Axios   
+                  </p>
                 </Col>
 
                 <Col md="4 text-center">
-                  <h1>one</h1>
+                  <h1>Database</h1>
+                  <p>
+                    MySQL, MongoDB 
+                  </p>
                 </Col>
 
 
 
               </Row>
-    
+
             </Jumbotron>
-      
+
           </div>
 
 
